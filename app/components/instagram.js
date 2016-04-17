67e2e5666fc7711d40119ef18e7fb982a9ea67ed
KerlinMichel <michelkerlin@gmail.com>
2016-04-16 20:16:20 -0400

    Can now pull image url from followers posts

--- app/components/instagram.js
+++ app/components/instagram.js
@@ -2,11 +2,13 @@ import React, {
   Component,
   View,
   Text,
-  WebView
+  WebView,
+  Image,
+  AsyncStorage
 } from 'react-native';
 
 var accessTokenURL = 'https://api.instagram.com/oauth/authorize/';
-var instagramURL = 'https://www.instagram.com/oauth/authorize/?client_id=378839e8b61f4c8ba6489c4cb8466b03&redirect_uri=https://github.com/instabuyTeam&response_type=token&scope=basic+public_content'
+var instagramURL = 'https://www.instagram.com/oauth/authorize/?client_id=378839e8b61f4c8ba6489c4cb8466b03&redirect_uri=https://github.com/instabuyTeam&response_type=token&scope=public_content+follower_list'
 
 
 class Instagram extends Component {
@@ -14,7 +16,11 @@ class Instagram extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      url : instagramURL
+      url: instagramURL,
+      token: '',
+      followers: '',
+      followers: [],
+      images: []
     }
   }
 
@@ -67,15 +73,92 @@ class Instagram extends Component {
         });
   }*/
 
+  componentDidMount() {
+
+  }
+
+  lookUpImage() {
+    fetch(accessTokenURL, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: JSON.stringify({
+        client_id: '378839e8b61f4c8ba6489c4cb8466b03',
+        redirect_uri: 'https://github.com/instabuyTeam',
+        response_type: 'token'
+      })
+
+    }).then(function (response) {
+      console.log(response);
+      console.log(response.json());
+          response.json().then(function(data){
+              console.log(data);
+          });
+        });
+  }
+
+  setImageList(data) {
+    var _this = this;
+    console.log(data);
+    console.log(data.data);
+    this.setState({followers: data.data});
+    for(var f in data.data) {
+      fetch('https://api.instagram.com/v1/users/' + data.data[f].id +'/media/recent?access_token='+this.state.token)
+        .then(response => response)
+        .then(res => res.json())
+        .then(data => _this.addImages(data));
+    }
+  }
+
+  addImages(data) {
+    console.log(data);
+    for(var i in data.data) {
+      var arr = this.state.images;
+      arr.push(data.data[i])
+      console.log(arr);
+      this.setState({images: arr});
+      console.log(this.state.images);
+    }
+  }
+
   webUpdate(navState) {
+    var _this = this;
     console.log(navState.url);
-    this.setState({url: navState.url})
+    this.setState({url: navState.url});
+    this.setState({token: navState.url.substring(navState.url.indexOf('=')+1, navState.url.length)});
+    AsyncStorage.setItem('access_token', navState.url.substring(navState.url.indexOf('=')+1, navState.url.length));
+    fetch('https://api.instagram.com/v1/users/self/follows?access_token='+this.state.token)
+      .then(response => response)
+      .then(res => res.json())
+      .then(data => _this.setImageList(data));
+
+    /*AsyncStorage.getItem("access_token", function(token) {
+      fetch('https://api.instagram.com/v1/users/self/follows?access_token='+token).then(function(response){
+        console.log(response);
+        console.log(response.json());
+        console.log(response.json()._65);
+        response.json().then(function(data){
+            console.log(data);
+        });
+      })
+    })*/
   }
 
   render() {
+    var images = this.state.images.map(function(item, key){
+      return(
+        <View key={key}>
+          <Text>{item.images.standard_resolution.url}</Text>
+          <Image style={{width: 50, height: 50}} source={{uri: item.images.standard_resolution.url}}/>
+        </View>
+      )
+    });
     return(
       <View>
         <Text>Instabuy</Text>
+        {images}
           <WebView
            onNavigationStateChange = {this.webUpdate.bind(this)}
            style={{
